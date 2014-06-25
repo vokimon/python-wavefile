55a8acdb62f5a84d921ad843e802d97bb719df60
David García Garzón <voki@canvoki.net>
2014-06-25 02:18:14 +0200

    Preparing for 1.2 development

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ https://github.com/vokimon/python-wavefile
 
 setup(
 	name = "wavefile",
-	version = "1.1",
+	version = "1.2~git",
 	description = "Pythonic wave file reader and writer",
 	author = "David Garcia Garzon",
 	author_email = "voki@canvoki.net",
